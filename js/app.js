d94ac24a530e0bea4a0203d9497ce3d9bb24d957
jasonsanders <jas.sanders@gmail.com>
2017-03-22 14:50:14 -0600

    trying to get the hosted version to work.  updated to the most recent angular and ui-router versions

--- js/app.js
+++ js/app.js
@@ -1,39 +1,38 @@
-angular.module('devmtnTravel', ['ui.router'])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home',{
-                url:'/',
-                templateUrl: '../views/about.html'
-            })
-            .state('adventurers',{
-                url:'adventurers',
-                parent:'home',
-                templateUrl: "../views/about-adventurers.html"
-            })
-            .state('contact',{
-                url:'contact',
-                parent:'home',
-                templateUrl: "../views/contact.html"
-            })
-            .state('packages',{
-                url:'/packages',
-                templateUrl: "../views/packages.html",
-                controller: 'packagesCtrl',
-                controllerAs: 'vm'
-            })
-            .state('booked',{
-                url:'/booked/:id',
-                templateUrl: "../views/booked.html",
-                controller: 'bookedCtrl',
-                controllerAs: 'vm'
-            })
-            .state('locations',{
-                url:'/locations',
-                templateUrl: "../views/locations.html",
-                controller: 'locationsCtrl',
-                controllerAs: 'vm'
-            });
+angular.module('devmtnTravel', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('home',{
+    url:'/',
+    templateUrl: '../views/about.html'
+  })
+  .state('adventurers',{
+    url:'adventurers',
+    parent:'home',
+    templateUrl: '../views/about-adventurers.html'
+  })
+  .state('contact',{
+    url:'contact',
+    parent:'home',
+    templateUrl: '../views/contact.html'
+  })
+  .state('packages',{
+    url:'/packages',
+    templateUrl: '../views/packages.html',
+    controller: 'packagesCtrl',
+    controllerAs: 'vm'
+  })
+  .state('booked',{
+    url:'/booked/:id',
+    templateUrl: '../views/booked.html',
+    controller: 'bookedCtrl',
+    controllerAs: 'vm'
+  })
+  .state('locations',{
+    url:'/locations',
+    templateUrl: '../views/locations.html',
+    controller: 'locationsCtrl',
+    controllerAs: 'vm'
+  });
 
-        $urlRouterProvider
-            .otherwise('/');
-    });
+  $urlRouterProvider
+  .otherwise('/');
+});
