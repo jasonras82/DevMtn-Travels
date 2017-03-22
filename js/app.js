d6f41ca2f9a7bf39b524b3fc2f4d5ecbeaa0fa7b
jasonsanders <jas.sanders@gmail.com>
2017-03-22 13:57:44 -0600

    when hosted, it can't find the files because it's not naming the project title in the url path, trying something.

--- js/app.js
+++ js/app.js
@@ -8,28 +8,28 @@ angular.module('devmtnTravel', ['ui.router'])
             .state('adventurers',{
                 url:'adventurers',
                 parent:'home',
-                templateUrl: "../views/about-adventurers.html"
+                templateUrl: "/views/about-adventurers.html"
             })
             .state('contact',{
                 url:'contact',
                 parent:'home',
-                templateUrl: "../views/contact.html"
+                templateUrl: "/views/contact.html"
             })
             .state('packages',{
                 url:'/packages',
-                templateUrl: "../views/packages.html",
+                templateUrl: "/views/packages.html",
                 controller: 'packagesCtrl',
                 controllerAs: 'vm'
             })
             .state('booked',{
                 url:'/booked/:id',
-                templateUrl: "../views/booked.html",
+                templateUrl: "/views/booked.html",
                 controller: 'bookedCtrl',
                 controllerAs: 'vm'
             })
             .state('locations',{
                 url:'/locations',
-                templateUrl: "../views/locations.html",
+                templateUrl: "/views/locations.html",
                 controller: 'locationsCtrl',
                 controllerAs: 'vm'
             });
